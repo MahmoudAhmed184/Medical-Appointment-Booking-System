f4e18448c55a9dcf4fedd319ece8a0e9da93b073
Nourhan Singer <nourhansingerr7@gmail.com>
2026-02-24 22:08:15 +0200

    feat: implement auth controller
    Wire register and login via catchAsync and authService.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,24 @@
-// TODO: Implement register handler
-const register = (req, res) => { };
+import catchAsync from '../utils/catchAsync.js';
+import * as authService from '../services/authService.js';
 
-// TODO: Implement login handler
-const login = (req, res) => { };
+const register = catchAsync(async (req, res) => {
+    const result = await authService.register(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: result,
+        message: 'Registration successful',
+    });
+});
+
+const login = catchAsync(async (req, res) => {
+    const result = await authService.login(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: result,
+        message: 'Login successful',
+    });
+});
 
 export { register, login };
