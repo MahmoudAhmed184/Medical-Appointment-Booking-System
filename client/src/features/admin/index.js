2ce9663a393b9c26859b33c950975cfafa8c0a36
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2026-02-26 07:22:24 +0200

    chore: update admin barrel exports

--- client/src/features/admin/index.js
+++ client/src/features/admin/index.js
@@ -1,6 +1,15 @@
+// Pages
 export { default as AdminDashboardPage } from './pages/DashboardPage';
 export { default as UsersPage } from './pages/UsersPage';
 export { default as SpecialtiesPage } from './pages/SpecialtiesPage';
 export { default as AdminAppointmentsPage } from './pages/AppointmentsPage';
+
+// Components
 export { default as UsersTable } from './components/UsersTable';
 export { default as SpecialtyForm } from './components/SpecialtyForm';
+
+// Hooks
+export { default as useUsers } from './hooks/useUsers';
+export { default as useSpecialties } from './hooks/useSpecialties';
+export { default as useAppointments } from './hooks/useAppointments';
+
