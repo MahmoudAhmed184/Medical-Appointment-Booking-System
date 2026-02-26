9ff3228214c4c292ce488e5e19a300d28dcedd88
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2026-02-26 07:16:56 +0200

    feat: implement admin custom hooks over Redux slices

--- client/src/features/admin/hooks/useSpecialties.js
+++ client/src/features/admin/hooks/useSpecialties.js
@@ -1,13 +1,59 @@
-// TODO: Implement useSpecialties hook
-import { useState } from 'react';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    fetchSpecialties,
+    createSpecialty,
+    updateSpecialty,
+    deleteSpecialty,
+    clearError,
+} from '../../../store/slices/adminSpecialtiesSlice';
 
 const useSpecialties = () => {
-    const [specialties, setSpecialties] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const { specialties, loading, error, formLoading, formError } = useSelector(
+        (state) => state.adminSpecialties
+    );
 
-    // TODO: Implement CRUD operations
+    const loadSpecialties = useCallback(() => {
+        dispatch(fetchSpecialties());
+    }, [dispatch]);
 
-    return { specialties, loading };
+    useEffect(() => {
+        loadSpecialties();
+    }, [loadSpecialties]);
+
+    const handleCreate = useCallback(
+        (data) => dispatch(createSpecialty(data)).unwrap(),
+        [dispatch]
+    );
+
+    const handleUpdate = useCallback(
+        (id, data) => dispatch(updateSpecialty({ id, data })).unwrap(),
+        [dispatch]
+    );
+
+    const handleDelete = useCallback(
+        (id) => dispatch(deleteSpecialty(id)).unwrap(),
+        [dispatch]
+    );
+
+    const handleClearError = useCallback(
+        () => dispatch(clearError()),
+        [dispatch]
+    );
+
+    return {
+        specialties,
+        loading,
+        error,
+        formLoading,
+        formError,
+        refetch: loadSpecialties,
+        createSpecialty: handleCreate,
+        updateSpecialty: handleUpdate,
+        deleteSpecialty: handleDelete,
+        clearError: handleClearError,
+    };
 };
 
 export default useSpecialties;
